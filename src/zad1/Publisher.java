27591c7c786571fb4c9b90f5fe530e073476e8b8
Michal Glaz <michal.glaz@op.pl>
2024-05-08 05:47:17 +0200

    Created addTopic and removeTopic methods for Publisher class

--- src/zad1/Publisher.java
+++ src/zad1/Publisher.java
@@ -1,4 +1,44 @@
 package zad1;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+
 public class Publisher {
+    public static void main(String[] args) throws IOException {
+        SocketChannel socketChannel = null;
+        String server = "localhost";
+        int serverPort = 50000;
+
+        try{
+            socketChannel = SocketChannel.open();
+            socketChannel.configureBlocking(false);
+            socketChannel.connect(new InetSocketAddress(server, serverPort));
+
+            while(!socketChannel.finishConnect()){
+
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Połączono");
+
+        Charset charset = Charset.forName("UTF-8");
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        CharBuffer charBuffer = null;
+
+//        socketChannel.write(charset.encode("Cześć jestem Adminem\n"));
+        addTopic("Polityka",socketChannel, charset);
+    }
+    public static void addTopic(String topic, SocketChannel socketChannel, Charset charset) throws IOException {
+        String topicJson = "{\"topic\":\""+topic+"\"}\n";
+        socketChannel.write(charset.encode(topicJson));
+    }
+    public static void removeTopic(String topic, SocketChannel socketChannel, Charset charset) throws IOException {
+        String topicJson = "{\"topic\":\""+topic+"\"}\n";
+        socketChannel.write(charset.encode(topicJson));
+    }
 }
