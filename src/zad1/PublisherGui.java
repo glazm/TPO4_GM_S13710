10970f7b114bc1abd7dcd9572717b706414fca79
Michal Glaz <michal.glaz@op.pl>
2024-05-11 09:55:14 +0200

    PublisherGui is done

--- src/zad1/PublisherGui.java
+++ src/zad1/PublisherGui.java
@@ -1,16 +1,15 @@
 package zad1;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.io.IOException;
-import java.util.ArrayList;
 
 public class PublisherGui extends JFrame{
     public Publisher publisher;
-//    public ArrayList<String> topicListValues = new ArrayList<String>();
     public PublisherGui(Publisher publisher){
         this.publisher = publisher;
         new JFrame("Publisher");
@@ -22,21 +21,21 @@ public class PublisherGui extends JFrame{
 
         JPanel addTopicPanel = new JPanel();
         addTopicPanel.setLayout(new GridBagLayout());
-//        addTopicPanel.setBorder(BorderFactory.createTitledBorder("Dodaj temat"));
-        GridBagConstraints addConstraints = new GridBagConstraints();
+        Border addTopicBorder = BorderFactory.createTitledBorder("Dodaj temat");
+        addTopicPanel.setBorder(addTopicBorder);
 
         JTextField topic = new JTextField();
-        addConstraints.fill = GridBagConstraints.HORIZONTAL;
-        addConstraints.weightx = 0.5;
-        addConstraints.weighty = 0.5;
-        addConstraints.gridy = 0;
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.weightx = 0.5;
+        constraints.weighty = 0.5;
+        constraints.gridy = 0;
 
-        addTopicPanel.add(topic,addConstraints);
+        addTopicPanel.add(topic,constraints);
 
         JButton addTopicButton = new JButton("Dodaj temat");
-        addConstraints.gridy = 1;
+        constraints.gridy = 1;
 
-        addTopicPanel.add(addTopicButton,addConstraints);
+        addTopicPanel.add(addTopicButton,constraints);
 
         constraints.gridy = 0;
 
@@ -44,66 +43,49 @@ public class PublisherGui extends JFrame{
 
         JPanel removeTopicPanel = new JPanel();
         removeTopicPanel.setLayout(new GridBagLayout());
-//        removeTopicPanel.setBorder(BorderFactory.createTitledBorder("Usuń temat"));
-        GridBagConstraints removeConstraints = new GridBagConstraints();
+
 
         JComboBox<String> topicList = new JComboBox();
-        removeConstraints.gridy = 0;
-        removeConstraints.gridx = 0;
+        Border topicListBorder = BorderFactory.createTitledBorder("Tematy");
+        topicList.setBorder(topicListBorder);
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridy = 0;
+        constraints.gridx = 0;
 
-        removeTopicPanel.add(topicList,removeConstraints);
+        removeTopicPanel.add(topicList,constraints);
 
         JButton removeTopicButton = new JButton("Usuń temat");
-        removeConstraints.gridy = 0;
-        removeConstraints.gridx = 1;
+        constraints.gridy = 0;
+        constraints.gridx = 1;
 
-        removeTopicPanel.add(removeTopicButton,removeConstraints);
+        removeTopicPanel.add(removeTopicButton,constraints);
 
         constraints.gridy = 1;
+        constraints.gridx = 0;
 
         mainPanel.add(removeTopicPanel,constraints);
 
         JPanel newsPanel = new JPanel();
         newsPanel.setLayout(new GridBagLayout());
-//        newsPanel.setBorder(BorderFactory.createTitledBorder("News"));
-//        GridBagConstraints newsConstraints = new GridBagConstraints();
 
-        JTextArea newsText = new JTextArea(10,10);
+        Border newsBorder = BorderFactory.createTitledBorder("Wyślij news'a");
+        newsPanel.setBorder(newsBorder);
+
+        JTextArea newsText = new JTextArea(10,50);
         newsText.setLineWrap(true);
         newsText.setWrapStyleWord(true);
-//        newsText.setPreferredSize(new Dimension(400,400));
-//        newsText.setMinimumSize(new Dimension(400, 400));
-//        newsText.setBounds(0, 0, 400, 400);
-//        newsConstraints.gridy = 0;
+
 
         newsPanel.add(new JScrollPane(newsText));
 
         JButton newsButton = new JButton("News");
-//        newsConstraints.gridy = 1;
 
         newsPanel.add(newsButton);
 
         constraints.gridy = 2;
+        constraints.gridx = 0;
         mainPanel.add(newsPanel,constraints);
 
-//        JLabel info = new JLabel("Tymczasowy tekst");
-////        info.setBorder(BorderFactory.createTitledBorder("Info"));
-//        constraints.gridy = 3;
-//        mainPanel.add(info,constraints);
-
-
-//        JTextField topic = new JTextField();
-//        constraints.fill = GridBagConstraints.HORIZONTAL;
-//        constraints.weightx = 0.5;
-//        constraints.weighty = 0.5;
-//        constraints.gridy = 0;
-
-//        mainPanel.add(topic, constraints);
-//
-//        JButton addTopicButton = new JButton("Dodaj temat");
-//        constraints.gridy = 1;
-//
-//        mainPanel.add(addTopicButton,constraints);
 
         addTopicButton.addActionListener(new ActionListener() {
             @Override
@@ -162,7 +144,7 @@ public class PublisherGui extends JFrame{
         this.add(mainPanel);
 
         this.pack();
-        this.setSize(500,300);
+        this.setSize(700,500);
         this.setVisible(true);
 
     }
@@ -170,25 +152,13 @@ public class PublisherGui extends JFrame{
     protected void processWindowEvent(WindowEvent e){
         super.processWindowEvent(e);
         if(e.getID() == WindowEvent.WINDOW_CLOSING){
-//            System.out.println("Będzie zamykane");
-//            for(String t:publisher.getTopics()){
-//                try {
-//                    System.out.println("Usuwanie: "+ t);
-//                    publisher.removeTopic(t);
-//                } catch (IOException ex) {
-//                    ex.printStackTrace();
-//                }
-//            }
+
             try {
                 publisher.removeAllTopics();
             } catch (IOException ex) {
                 ex.printStackTrace();
             }
-//            try {
-//                Thread.sleep(200);
-//            } catch (InterruptedException ex) {
-//                ex.printStackTrace();
-//            }
+
             publisher.closingPublisher();
             System.exit(0);
         }
